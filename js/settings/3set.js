0e080dd1eb46100d5ae45866dafcec3c5154df5b
Galactic Network <114846211+GalacticNetwork@users.noreply.github.com>
2023-02-11 19:57:23 +0000

    Big update + release

---
+++ js/settings/3set.js
@@ -0,0 +1,40 @@
+console.log("3kh0.github.io")
+
+console.log("Running!");
+//Declare variables for cloak here
+const local_title = localStorage.getItem("title");
+const local_icon = localStorage.getItem("icon");
+
+//If the window already has title stored in localstorage
+if (window.localStorage.hasOwnProperty("title")) {
+  document.title = local_title;
+}
+//Fetch and set from user's input
+if (window.localStorage.hasOwnProperty("icon")) {
+  document.querySelector("link[rel=icon]").href = local_icon;
+  document.querySelector("link[rel='shortcut icon']").href = local_icon;
+  document.querySelector("link[rel*='icon']").href = local_icon;
+}
+
+//Add Google Analytics
+const gascript = document.createElement("script");
+gascript.setAttribute("async", "");
+gascript.setAttribute("src", "https://www.googletagmanager.com/gtag/js?id=G-98DP5VKS42");
+const inlinegascript = document.createElement("script");
+inlinegascript.innerHTML = `window.dataLayer = window.dataLayer || [];
+function gtag(){dataLayer.push(arguments);}
+gtag('js', new Date());
+gtag('config', 'G-98DP5VKS42');`
+document.body.append(gascript, inlinegascript);
+console.log("Google Analytics added");
+
+//Turn off GSAP null warnings (if present)
+try {
+  gsap.config({
+    nullTargetWarn: false,
+  });
+} catch {
+  console.log("empty b/c no need for return :)");
+}
+
+// Credit to Team r0cket for the original code :)
